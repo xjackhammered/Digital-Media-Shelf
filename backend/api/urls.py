c4abb52eee17941b570d787e53c592dc5f149b8c
xjackhammered <hammerofjack7@gmail.com>
2025-07-31 11:01:28 +0600

    configured views for Genre

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('add-media', views.addMedia),
     path('delete-media/<int:id>/', views.deleteMedia),
     path('update-media/<int:id>/', views.updateMedia),
+    path('all-genre/', views.allGenres),
+    path('add-genre/', views.addGenre),
+    path('delete-genre/<int:id>/', views.deleteGenre),
+    path('update-genre/<int:id>/', views.updateGenre),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.response import Response # pyright: ignore[reportMissingImports]
+from rest_framework.decorators import api_view # pyright: ignore[reportMissingImports]
 from .serializers import MediaSerializer, GenreSerializer, ReviewSerializer
-from .models import MediaItem
+from .models import MediaItem, Genre, Review
 # Create your views here.
 
 @api_view(['GET'])
@@ -38,4 +38,35 @@ def updateMedia(request, id):
 
     return Response(serializer.data)
 
+@api_view(['GET'])
+def allGenres(request):
+    genres = Genre.objects.all()
+    serializer = GenreSerializer(genres, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addGenre(request):
+    serializer = GenreSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    
+    return Response(serializer.data)
+
+@api_view(['POST', 'PATCH'])
+def updateGenre(request, id):
+    genre = Genre.objects.get(id=id)
+
+    partial = request.method == 'PATCH'
+    serializer = GenreSerializer(instance=genre, data=request.data, partial=partial)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteGenre(request, id):
+    genre = Genre.objects.get(id=id)
+    genre.delete()
+    return Response("Genre has been successfully deleted!")
 
